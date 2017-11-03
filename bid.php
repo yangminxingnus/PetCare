5dbf59bcfb61e5a61592e2fe120a836f66209e0e
yangminxingnus <e0134082@u.nus.edu>
2017-11-03 19:24:51 +0800

    mine

--- bid.php
+++ bid.php
@@ -1,13 +1,34 @@
 <?php
 	date_default_timezone_set('Asia/Singapore');
 	require_once("dbh.inc.php");
+    require_once("text.inc.php");
+
 	include 'header.php';
 ?>
 
-<?php
-echo "This is the bid page!";
-get($conn);
-?>
+<?php 
+echo "
+    <div>
+    <form class='form-signin' action='".search($conn)." ".bid($conn)."'  method='POST'>
+        <h2 class='form-signin-heading'>Key in details to find your pet carer</h2>
+        <input type='text' name='petType'class='form-control'placeholder='pet type' required ><br>
+        <input type='date' name='sTime' class='form-control'placeholder='start time' required ><br>
+        <input type='date' name='eTime' class='form-control' placeholder='end time' required ><br>
+        <button class='btn btn-warning btn-block' type='submit' name='searchASubmit'>Search</button>
+    </form>
+    </div>
 
+            <div>
+                  <form class='form-signin' method='POST'>
+                  <input type='text' name='aId'class='form-control'placeholder='availiability id' required ><br>
+                   <input type='text' name='bidPoints'class='form-control'placeholder='your points' required ><br>
+                   <input type='text' name='petId'class='form-control'placeholder='your pet Id' required ><br>
+                   <button class='btn btn-warning btn-block' type='submit' name='bidSubmit'>Bid</button>
+                  </form>
+
+            </div>"; 
+
+
+?>
 </body>
-</html>
+</html> 
--- text.inc.php
+++ text.inc.php
@@ -40,6 +40,68 @@ function logout(){
 	}
 }
 
+//search
+function search($conn){
+    if (isset($_POST['searchASubmit'])){
+		$pType = $_POST['petType'];
+		$sTime = $_POST['sTime'];
+		$eTime = $_POST['eTime'];
+		$sql = "SELECT * FROM availability a WHERE a.ptype = '$pType' AND ((a.afrom <= '$sTime' AND a.ato >= '$eTime'))";
+		
+		$result = pg_query($conn, $sql);   
+		while ($row = pg_fetch_assoc($result)) {
+           
+			echo "<div class='panel panel-warning'>
+
+			<div class='panel panel-heading'><h3>";
+			echo $row['aid']."</h3>";
+			echo "</div>
+
+			<div class='panel panel-body' method='POST'>";
+  		    echo $row['cid']."</h3>". "        " .$row['uid']."</h3>". "        " .$row['ptype']."</h3>". "        " .$row['afrom']."</h3>". "        " .$row['ato']."</h3>";
+		    
+		    echo "
+           <div>
+                  <form class='form-signin' action='".bid($conn)."' method='POST'>
+                  <input type='text' name='aId'class='form-control'placeholder='availability' required ><br>
+                   <input type='text' name='bidPoints'class='form-control'placeholder='your points' required ><br>
+                   <input type='text' name='petId'class='form-control'placeholder='your pet Id' required ><br>
+                   <button class='btn btn-warning btn-block btn-xs' type='submit' name='bidSubmit'>Bid</button>
+                  </form>
+
+            </div>"; 
+
+			echo "</div></div>";
+	    }
+	}
+}
+
+//search
+function bid($conn){
+    if (isset($_POST['bidSubmit'])){	
+    	$points = $_POST['bidPoints'];
+        $petId = $_POST['petId'];
+        $uid = $_SESSION['uid'];
+
+	    // $result2 = pg_query($conn, "SELECT p.pid FROM pets p, users u WHERE u.uid = p.oid AND p.pid = '$petId'");
+	    // $row = pg_fetch_assoc($result2);
+	    // $pid = $row[pid];
+		$sql = "INSERT INTO bid VALUES ('$uid', '$aid', '$petId', 'pending', $points)";
+		$result = pg_query($conn, $sql);  
+		if (!$result) {
+			echo "<div class='alert alert-danger alert-dismissible' role='alert'>
+			  Bid failed. $sql
+			</div>";
+		} else {
+			//header("Location: index.php");
+			echo "<div class='alert alert-success alert-dismissible' role='alert'>
+			  Bid successfully!
+			</div>";
+		}
+	}
+}
+
+
 //submit the sign up information
 function sign_up($conn){
 	if (isset($_POST['signupSubmit'])){
@@ -62,5 +124,36 @@ function sign_up($conn){
 		
 	}
 }
+
+
+//get the infomation of the blogs
+function get($conn){
+	$sql = "SELECT * FROM texts";
+	$result = mysqli_query($conn, $sql);
+	while ($row = mysqli_fetch_assoc($result)) {
+		echo "<div class='panel'><div class='panel panel-heading'><h3>";
+		echo $row['uid']."</h3>"."   ".$row['date'];
+		echo "</div><div class='panel panel-body'>";
+		echo $row['message'];
+		if(isset($_SESSION['uid'])){
+			if($_SESSION['uid']==$row['uid']){
+				echo "<form class='delete-form' method='POST' action='".delete($conn)."'>
+				<input type='hidden' name='id' value='".$row['id']."'>
+				<button type=submit name = 'TextDelete' class='btn btn-warning btn-xs'>DELETE</button>
+			</form>
+			<form class='edit-form' method='POST' action='editText.php'>
+				<input type='hidden' name='id' value='".$row['id']."'>
+				<input type='hidden' name='uid' value='".$row['uid']."'>
+				<input type='hidden' name='date' value='".$row['date']."'>
+				<input type='hidden' name='message' value='".$row['message']."'>
+				<button class='btn btn-warning btn-xs'>EDIT</button>
+			</form>";
+			} 
+		}
+		echo "</div></div>";
+	}
+	
+}
+
 
 ?>
