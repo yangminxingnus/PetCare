d3c9e30d830e6283ca05b161b65c098c7f3a08bb
Chen Xiaoman <836932889@qq.com>
2017-11-05 19:49:39 +0800

    Can show status in bidder list

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = pg_connect("host=localhost port=5432 dbname=PetCare user=postgres password=3154");
+$conn = pg_connect("host=localhost port=5432 dbname=PetCare user=postgres password=1124");
 
 if (!$conn) {
 	die('Connection failed.??');
